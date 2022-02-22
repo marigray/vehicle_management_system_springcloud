d98720bde7412184e0527cd5b78cb7aebe9f1890
Yang <179837740@qq.com>
2022-02-22 19:26:13 +0800

    添加启动类

---
+++ car_lending/src/main/java/com/ycx/lend/LendingBoot.java
@@ -0,0 +1,18 @@
+package com.ycx.lend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @Author ycx
+ * @Date 2022/2/22 19:22
+ * @Description
+ */
+@SpringBootApplication
+@MapperScan(basePackages = {"com.ycx.lend.mapper"})
+public class LendingBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(LendingBoot.class,args);
+    }
+}

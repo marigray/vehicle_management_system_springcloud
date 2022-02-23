3cbd569469a910f7003d8282880be2d8e99510a2
1772347717 <81063969+1772347717@users.noreply.github.com>
2022-02-23 21:48:59 +0800

    配置Eureka

--- vire/src/main/java/com/wang/vire/vireApplication.java
+++ vire/src/main/java/com/wang/vire/vireApplication.java
@@ -3,10 +3,14 @@ package com.wang.vire;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.wang.vire.mapper"})
+@EnableEurekaClient
+@EnableFeignClients
 public class vireApplication {
     public static void main(String[] args) {
         SpringApplication.run(vireApplication.class,args);

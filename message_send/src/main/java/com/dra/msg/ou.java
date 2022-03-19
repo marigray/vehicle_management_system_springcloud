3405a4d4cae31de17dbfa4c50afd88c2c2cd9e5c
dra <draso@vip.qq.com>
2022-03-19 21:30:09 +0800

    更新相关配置

--- message_send/src/main/java/com/dra/msg/ou.java
+++ message_send/src/main/java/com/dra/msg/ou.java
@@ -1,9 +1,8 @@
 package com.dra.msg;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ou {
     public static void main(String[] args) {
         SpringApplication.run(ou.class,args);
